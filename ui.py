a912186864a3e066f9a99485312e2137ef7acc0d
KKB_Official <kkboahene003@gmail.com>
2025-08-28 11:37:33 -0700

    some edits

--- ui.py
+++ ui.py
@@ -3,6 +3,13 @@ from functools import partial
 from tkinter import messagebox
 import commands
 
+def handle_update(id, title, app):
+    if not title:
+        messagebox.showerror(title="Edit task", message="Cannot update with an empty task!", parent=app)
+    else:
+        commands.update_task(id, {"title": title})
+        show_all_tasks_frame(app)
+
 def handle_delete(id, app):
     commands.delete_task(id)
     show_all_tasks_frame(app)
@@ -17,6 +24,36 @@ def submit_task(title, app):
     else:
         commands.save_task({"title": title})
         show_all_tasks_frame(app)
+    
+def show_edit_task_frame(task, app):
+    frame = tk.Frame(master=app)
+    frame.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+
+    label = tk.Label(master=frame, text=f"Edit task: {task["title"]}")
+    label.grid()
+    # Add an entry widget and show the task title
+    # Add a button with text Update for saving the changes
+    # Add a button with text Back / Cancel for remove the frame
+
+    task_entry = tk.Entry(master=frame)
+    task_entry.insert(0, task["title"])
+    task_entry.grid(column=1)
+
+    
+    update_btn = tk.Button(
+        master=frame,
+        text = "Update",
+        command=lambda: handle_update(task["_id"], task_entry.get(), app))
+    update_btn.grid(row=2)
+
+    cancel_btn = tk.Button(
+        master=frame,
+        text = "Back",
+        command=lambda: frame.destroy())
+    cancel_btn.grid(row=2, column=1)
+
+    frame.tkraise()
+
 
 def show_add_ask_frame(app):
     frame = tk.Frame(master=app)
@@ -42,7 +79,8 @@ def show_all_tasks_frame(app):
 
         edit_btn = tk.Button(
             master=frame,
-            text="Edit")
+            text="Edit",
+            command=partial(show_edit_task_frame, task, app))
         edit_btn.grid(row=tasks.index(task), column=1)
 
         delete_btn = tk.Button(
